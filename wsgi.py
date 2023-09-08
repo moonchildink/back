82b4a5512c934456139aaef6f4603123e3a998d0
moonchild <carise0102@gmail.com>
2023-09-08 15:08:16 +0800

    根据系统环境使用不同配置

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
-from app import creat_app,db
+from app import creat_app, db
+import os
 
-app = creat_app()
-
+os_name = os.name
+if os_name == 'nt':
+    app = creat_app()
+elif os_name == 'posix':
+    app = creat_app(config_name='deploy')
 
 with app.app_context():
     db.create_all()
